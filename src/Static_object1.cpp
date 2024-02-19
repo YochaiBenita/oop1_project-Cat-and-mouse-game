7b76f3c17fee0616f1a16b1055e5c681bbc22eff
Yochai <yochai.benita@gmail.com>
2024-02-19 21:10:41 +0200

    lllll

---
+++ src/Static_object1.cpp
@@ -0,0 +1,13 @@
+#include "Static_object.h"
+
+
+Static_object::Static_object(object_code obj, int col, int row)
+	: Object(m_texures[obj], col, row)
+{
+	//
+}
+
+
+Static_object::~Static_object()
+{
+}
