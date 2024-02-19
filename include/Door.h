a1118f377a38a74e387a38448b1959e62bc3c3c6
Yochai <yochai.benita@gmail.com>
2024-02-19 19:45:55 +0200

    מעבר בניין

--- include/Door.h
+++ include/Door.h
@@ -7,13 +7,13 @@
 class Door :public Static_object
 {
 public:
-	Door(int col, int row):Static_object();
+	Door(int col, int row) :Static_object(door, col, row) {};
 	virtual ~Door();
 	virtual void draw(sf::RenderWindow&)const override;
 
 	void handleCollision(Object& obj) { obj.handleCollision(*this); }
-	void handleCollision(Cat& cat) { cat.handleCollision(*this); }
-	void handleCollision(Mouse& mouse) { mouse.handleCollision(*this); }
+	void handleCollision(Cat& catPlayer) { catPlayer.handleCollision(*this); }
+	void handleCollision(Mouse& mousePlayer) { mousePlayer.handleCollision(*this); }
 
 private:
 
