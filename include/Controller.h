caee58b9ded67b977a01016509e4759da97b556d
Benjamin Rosin <benjaminro@std.hac.ac.il>
2024-02-19 18:08:56 +0200

    bbb

--- include/Controller.h
+++ include/Controller.h
@@ -5,11 +5,24 @@
 #include <fstream>
 #include "Level.h"
 
+const int NUM_OF_TXTR = 7;
+const std::string FileNames[NUM_OF_TXTR] = {
+	"mouse.png",
+	"cat.png",
+	"chease.png",
+	"door.png",
+	"gift.png",
+	"key.png",
+	"wall.png",
+};
 
 class Controller {
 public:
 	Controller();
 	~Controller();
+	void play();
+
+	
 
 private:
 	int m_life = 3;
@@ -18,4 +31,5 @@ private:
 
 	Level* m_currLevel;
 	const std::vector<std::string> m_levels_name;
+	const sf::Texture* m_texures;
 };
