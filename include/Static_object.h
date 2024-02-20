1d7221c228fd3de2a0f2da7d198b5cbe5c792a2c
Yochai <yochai.benita@gmail.com>
2024-02-20 10:41:57 +0200

    תחילת עבודה על התנגשויות

--- include/Static_object.h
+++ include/Static_object.h
@@ -2,13 +2,26 @@
 #include <SFML/Graphics.hpp>
 #include "Object.h"
 #include "Resources.h"
+
 class Static_object :public Object {
 public:
 	Static_object(object_code, int, int);
 	virtual ~Static_object();
 
-	virtual void draw(sf::RenderWindow&)=0;
-	void move(sf::Vector2f) {};
+	virtual void draw(sf::RenderWindow&);
+	virtual void move(sf::Vector2f) {};
+
+	virtual void handleCollision(Object&) = 0;
+	virtual void handleCollision(Mouse&) = 0;
+	virtual void handleCollision(Cat&) = 0;
+	virtual void handleCollision(Cheese&) {};
+	virtual void handleCollision(Door&) {};
+	virtual void handleCollision(Gfreeze&) {};
+	virtual void handleCollision(Gift&) {};
+	virtual void handleCollision(Glife&) {};
+	virtual void handleCollision(Gtime&) {};
+	virtual void handleCollision(Key&) {};
+	virtual void handleCollision(Wall&) {};
 
 private:
 
